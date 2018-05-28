8faa3c1ca024afbb075f18477dd468e30556d034
HTiet <heathertiet@gmail.com>
2018-05-27 22:44:28 -0400

    updated Form component and styled

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import BucketForm from './BucketForm.js';
 
 class App extends Component {
   render() {
@@ -8,6 +9,9 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">My Bucket List</h1>
         </header>
+
+        <BucketForm />
+
       </div>
     );
   }
--- src/BucketForm.js
+++ src/BucketForm.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import './BucketForm.css';
 
 class BucketForm extends React.Component {
     render() {
         return (
             <div>
-
+                <form>
+                    <div className="form-row">
+                        <div className="col">
+                            <input type="text" className="form-control" placeholder="Add your bucket list goal!" />
+                        </div>
+                        <button type="button" className="btn btn-info">Add</button>
+                    </div>
+                </form>
             </div>
         );
     }
